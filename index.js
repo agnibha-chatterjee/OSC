4594c44b16ab152ddea899c5e962faec50af07b2
Agnibha Chatterjee <agni.bhaaa@gmail.com>
2018-09-27 13:23:41 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(port, '127.0.0.1', () => {
+  console.log(`Server running on port ${port}`);
+});
